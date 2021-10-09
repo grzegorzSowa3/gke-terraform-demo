fe21d95a525aa7e06d83392900e1c9657ed6bbc4
grzeg <grzegorz.sowa314@gmail.com>
2021-10-09 13:36:54 +0200

    move project-id to variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project     = "project-id"
-  region      = "europe-central2"
-  zone      = "europe-central2-a"
+  project = "project-id"
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
+  name                     = "my-gke-cluster"
   remove_default_node_pool = true
   initial_node_count       = 1
 }
@@ -18,11 +18,5 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
   }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+  default = "europe-central2"
+}
+
+variable "zone" {
+  type = string
+  default = "europe-central2-a"
+}
