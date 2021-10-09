a1d816a5c488a8898701d0f8a4527a2642570b67
grzeg <grzegorz.sowa314@gmail.com>
2021-10-09 13:43:13 +0200

    add service account

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,11 @@ provider "google" {
   zone    = var.zone
 }
 
+resource "google_service_account" "default" {
+  account_id   = "k8s-service-account-id"
+  display_name = "K8s Service Account"
+}
+
 resource "google_container_cluster" "primary" {
   name                     = "my-gke-cluster"
   remove_default_node_pool = true
@@ -18,5 +23,10 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
+
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
