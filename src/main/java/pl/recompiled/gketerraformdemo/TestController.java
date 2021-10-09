8dac69bcf6537314d65b1514f19c3482d43d2796
grzeg <grzegorz.sowa314@gmail.com>
2021-10-09 14:33:31 +0200

    add test controller

---
+++ src/main/java/pl/recompiled/gketerraformdemo/TestController.java
@@ -0,0 +1,13 @@
+package pl.recompiled.gketerraformdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "TEST RESPONSE FROM K8S APP";
+    }
+}
