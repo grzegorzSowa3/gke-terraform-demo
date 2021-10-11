1224e4b23a010ddc4a4f82274cdb08833bf68c56
grzeg <grzegorz.sowa314@gmail.com>
2021-10-12 00:00:17 +0200

    fix postgres database

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
 resource "google_sql_database_instance" "postgres" {
   name = "postgres"
-  project_id = var.project_id
-  zone = var.zone
+  project = var.project_id
+  region = var.region
   database_version = "POSTGRES_13"
 }
