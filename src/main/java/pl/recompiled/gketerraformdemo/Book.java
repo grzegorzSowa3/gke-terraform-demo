09891c8d2d6bf2aa13d4474b019b46caa5cc1b54
grzeg <grzegorz.sowa314@gmail.com>
2021-10-17 22:22:19 +0200

    add getters to Book

--- src/main/java/pl/recompiled/gketerraformdemo/Book.java
+++ src/main/java/pl/recompiled/gketerraformdemo/Book.java
@@ -1,6 +1,7 @@
 package pl.recompiled.gketerraformdemo;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Book {
 
