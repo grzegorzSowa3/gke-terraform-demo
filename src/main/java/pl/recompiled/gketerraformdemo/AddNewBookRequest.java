f461db2a5277bb135861b5ed72845d8b4500419f
grzeg <grzegorz.sowa314@gmail.com>
2021-10-17 22:19:35 +0200

    remove all args constructor from AddNewBookRequest

--- src/main/java/pl/recompiled/gketerraformdemo/AddNewBookRequest.java
+++ src/main/java/pl/recompiled/gketerraformdemo/AddNewBookRequest.java
@@ -1,10 +1,8 @@
 package pl.recompiled.gketerraformdemo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class AddNewBookRequest {
 
     private String title;
